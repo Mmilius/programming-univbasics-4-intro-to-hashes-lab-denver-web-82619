5790005139bb0dbe37f82060d32889ec19f2dc56
Martin Milius <martin.milius@gmail.com>
2019-08-13 02:46:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,12 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash 
+  ha
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  if hash{:key}
+    puts
+  else
+    puts
   hash
 end
