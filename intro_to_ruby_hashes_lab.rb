573fa20e7ba9f84bbbacbc22d872f590b7f0cbc3
Martin Milius <martin.milius@gmail.com>
2019-08-13 03:20:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,10 +26,8 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-    puts hash[key] = 1
+    hash[key] += 1
   else
-    puts "Key not fount!"
-    if hash[key] hash[key] += 1
-    else hash[key] = 1
+    hash[key] = 1
 end
 end
